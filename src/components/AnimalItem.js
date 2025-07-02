3ed57d79a35b6d06c6eab6427be0d8c5fca15f4e
73962869 <taeyeol2483@gmail.com>
2025-07-02 18:02:00 +0900

    Update App.js and components for cat image API integration

--- src/components/AnimalItem.js
+++ src/components/AnimalItem.js
@@ -1,9 +1,8 @@
 const AnimalItem = ({ src, alt }) => {
-  console.log('process.env.PUBLIC_URL >> ', process.env.PUBLIC_URL);
 
   return (
     <li>
-      <img src={`${process.env.PUBLIC_URL}/${src}`} alt={alt} />
+      <img src={src} alt={alt} />
     </li>
   );
 }
--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,10 +1,9 @@
 const MainCard = ({ src, alt, handleHeartClick, choiceFavorite }) => {
-
   const heartIcon = choiceFavorite ? '🐱‍🐉' : '🤡'
-
+  
   return (
     <div className="main-card">
-      <img src={`${process.env.PUBLIC_URL}/${src}`} alt={alt} width="400px" />
+      <img src={src} alt={alt} width="400px" />
       <button onClick={handleHeartClick}>{heartIcon}</button>
     </div>
   );
